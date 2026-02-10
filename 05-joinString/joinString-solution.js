99d9c54da72fc2ba3700457c8c4062e9ead9ebff
ael-n <edwidgearie@gmail.com>
2026-02-10 11:26:38 -0800

    joinstring

---
+++ 05-joinString/joinString-solution.js
@@ -0,0 +1,21 @@
+const firstName = Carlos;
+const lastName = Stevenson;
+const birthYear = 1947;
+const thisYear = 1965;
+const fullName = firstName + lastName;
+const age = thisYear - birthYear;
+
+const greeting = {"Hello my name is " + fullName + ", i am" + age + "years old."}
+
+console.log (greeting);
+
+module.exports = {
+    firstName: typeof firstName === 'undefined' ? undefined : firstName,
+    lastName: typeof lastName === 'undefined' ? undefined : lastName,
+    thisYear: typeof thisYear === 'undefined' ? undefined : thisYear,
+    birthYear: typeof birthYear === 'undefined' ? undefined : birthYear,
+    greeting: typeof greeting === 'undefined' ? undefined : greeting,
+    fullName: typeof fullName === 'undefined' ? undefined : fullName,
+    age: typeof age === 'undefined' ? undefined : age
+}
+}
