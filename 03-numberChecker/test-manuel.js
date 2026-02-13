dc8cab56777e8fd165fdd9f264e0a25b2083503f
ael-n <edwidgearie@gmail.com>
2026-02-13 13:00:33 -0800

    add file for the test

---
+++ 03-numberChecker/test-manuel.js
@@ -0,0 +1,6 @@
+﻿const numberChecker = require('./numberChecker-solution.js');
+
+console.log('Test avec 1000:', numberChecker(1000));
+console.log('Test avec 10:', numberChecker(10));
+console.log('Test avec 9:', numberChecker(9));
+console.log('Test avec 6:', numberChecker(6));
